68c4b97885104d435c7d46f1be05d87e5ca4d6da
saiple <32297210+saiple@users.noreply.github.com>
2021-03-08 18:18:55 +0300

    Update 2. Tokens

--- 2. Tokens/main.py
+++ 2. Tokens/main.py
@@ -27,15 +27,16 @@ files_len = len(sites)
 for i in range(files_len):
     file = sites[i]
     print('Processing site ' + str(i) + '/' + str(files_len) + '. ' + file)
-    html = open(sites_path + '/' + file, "r", encoding="utf-8")
+    html_file = open(sites_path + '/' + file, "r", encoding="utf-8")
+    html = html_file.read().replace("<br>", " ")
+    html_file.close()
     parsed_html = BeautifulSoup(html, features="html.parser")
     sentence = re.sub(r"[\n\s.,:–\\?—\-!()/»+©\"]+", " ", parsed_html.text, flags=re.UNICODE).lower() # makes normalization faster
     tokens = [token for token in sentence.split(" ") if token not in russian_stopwords \
               and token != " " \
               and token.strip() not in punctuation and len(token) > 1]
     words.extend(tokens)
-    html.close()
-
+    
 words_file = open(words_path, "a", encoding="utf-8")
 words_dict = Counter(words)
 
@@ -62,7 +63,7 @@ tokens_file = open(tokens_path, "a", encoding="utf-8")
 for key, words_tokens in tokens.items():
     tokens_file.write(key + " ")
     for word in words_tokens:
-        if word != key:
+        if word != key and len(word) > 1:
             tokens_file.write(word + " ")
     tokens_file.write("\n")
 
